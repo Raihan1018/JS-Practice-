89ff5e800fdca24c4f8ecb219adcaea5e332c5f2
Raihan <raihanuddinahmed10@gmail.com>
2025-07-07 22:20:44 +0600

    some problem solve qus with ans

---
+++ practice.js
@@ -0,0 +1,36 @@
+/**
+1. Complex Discount Calculator
+Given a price and isMember (boolean), apply the following rules:
+
+-> If member and price > 500 → 20% discount
+-> If member and price <= 500 → 10% discount
+-> If not member → no discount
+-> Output the final price after discount.
+ */
+
+// bad way -> not recommended this way
+let price = 600;
+const isMember = false;
+
+if (isMember === true && price > 500) {
+  discountPrice = 0.2 * price;
+  newPrice = price - discountPrice;
+  console.log(newPrice);
+}
+if (isMember === true && price <= 500) {
+  discountPrice = 0.1 * price;
+  newPrice = price - discountPrice;
+  console.log(newPrice);
+}
+if (isMember === false) {
+  console.log(price);
+}
+
+// Optimism way
+
+let discount = 0;
+if (isMember) {
+  discount = price > 500 ? 0.2 : 0.1;
+}
+const newPrice = price - price * discount;
+console.log(`After discount new price is:  ${newPrice}`);
