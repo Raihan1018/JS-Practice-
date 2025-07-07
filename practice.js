3a73a3ce72575beaa6cae95f65d59379eba1c75e
Raihan <raihanuddinahmed10@gmail.com>
2025-07-08 01:27:22 +0600

    even odd

--- practice.js
+++ practice.js
@@ -9,31 +9,31 @@ Given a price and isMember (boolean), apply the following rules:
  */
 
 // bad way -> not recommended this way
-let price = 600;
-const isMember = true;
+// let price = 600;
+// const isMember = true;
 
-if (isMember === true && price > 500) {
-  discountPrice = 0.2 * price;
-  newPrice = price - discountPrice;
-  console.log(newPrice);
-}
-if (isMember === true && price <= 500) {
-  discountPrice = 0.1 * price;
-  newPrice = price - discountPrice;
-  console.log(newPrice);
-}
-if (isMember === false) {
-  console.log(price);
-}
+// if (isMember === true && price > 500) {
+//   discountPrice = 0.2 * price;
+//   newPrice = price - discountPrice;
+//   console.log(newPrice);
+// }
+// if (isMember === true && price <= 500) {
+//   discountPrice = 0.1 * price;
+//   newPrice = price - discountPrice;
+//   console.log(newPrice);
+// }
+// if (isMember === false) {
+//   console.log(price);
+// }
 
 // Optimism way
 
-let discount = 0;
-if (isMember) {
-  discount = price > 500 ? 0.2 : 0.1;
-}
-const newPrice = price - price * discount;
-console.log(`After discount new price is:  ${newPrice}`);
+// let discount = 0;
+// if (isMember) {
+//   discount = price > 500 ? 0.2 : 0.1;
+// }
+// const newPrice = price - price * discount;
+// console.log(`After discount new price is:  ${newPrice}`);
 
 /**
 2. Nested Condition for Travel Eligibility
@@ -44,17 +44,32 @@ Given passport (boolean), visa (boolean), and ticket (boolean), check:
 -> If none → "Not eligible for travel"
  */
 
-const passport = false;
-const visa = false;
-const ticket = false;
+// const passport = false;
+// const visa = false;
+// const ticket = false;
 
-if (passport && visa && ticket) {
-  console.log("Can travel");
-} else if (!passport && !visa && !ticket) {
-  console.log("Not eligible for travel");
-} else {
-  console.log("Missing:");
-  if (!passport) console.log("- Passport");
-  if (!visa) console.log("- Visa");
-  if (!ticket) console.log("- Ticket");
+// if (passport && visa && ticket) {
+//   console.log("Can travel");
+// } else if (!passport && !visa && !ticket) {
+//   console.log("Not eligible for travel");
+// } else {
+//   console.log("Missing:");
+//   if (!passport) console.log("- Passport");
+//   if (!visa) console.log("- Visa");
+//   if (!ticket) console.log("- Ticket");
+// }
+
+
+
+// Qus:
+  // Even or Odd Checker
+  // Print numbers from 1 to 50. For each number, print whether it is "Even" or "Odd".
+
+for(let i = 1; i<=50; i++){
+  if(i %2 === 0){
+    console.log(i, 'Even')
+  }
+  else{
+    console.log(i, 'Odd')
+  }
 }
