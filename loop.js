fc8e5976327a7a4005d75851565d37b534d4d04a
Raihan <raihanuddinahmed10@gmail.com>
2025-07-07 22:04:35 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,19 @@
+// Ternary
+
+/**
+ * structure -> condition ? true_value : false_value
+ */
+
+// normal way
+const age = 1;
+// if (age >= 18){
+//     console.log('Voter')
+// }
+// else{
+//     console.log('Not voter')
+// }
+
+// ternary way
+
+age = age >= 18 ? 'voter' : 'Not voter'
+console.log(age)
